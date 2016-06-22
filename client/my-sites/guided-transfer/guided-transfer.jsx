dbd08b2c9b7eae51ab668f36cd52b06633caf972
Omar Jackman <github@omarjackman.com>
2016-06-21 21:00:32 -0400

    work in progress

--- client/my-sites/guided-transfer/guided-transfer.jsx
+++ client/my-sites/guided-transfer/guided-transfer.jsx
@@ -8,11 +8,13 @@ import page from 'page';
  * Internal dependencies
  */
 import HeaderCake from 'components/header-cake';
+import AccountInfo from './account-info';
 
 export default React.createClass( {
 	displayName: 'GuidedTransfer',
 
 	propTypes: {
+		hostSlug: PropTypes.string,
 		siteSlug: PropTypes.string.isRequired
 	},
 
@@ -20,10 +22,22 @@ export default React.createClass( {
 		page( `/settings/export/${this.props.siteSlug}` );
 	},
 
+	getHostInfo( host ) {
+		switch ( host ) {
+			case 'bluehost': return {
+				label: this.translate( 'Bluehost' ),
+				url: '#bluehost'
+			};
+		}
+	},
+
 	render: function() {
+		const hostInfo = this.getHostInfo( this.props.hostSlug );
+
 		return (
 			<div className="guided-transfer">
-				<HeaderCake onClick={ this.showExporter } isCompact={ true }>{ this.translate( 'Guided Transfer' ) }</HeaderCake>
+				<HeaderCake onClick={ this.showExporter }>{ this.translate( 'Guided Transfer' ) }</HeaderCake>
+				{ hostInfo && <AccountInfo hostInfo={ hostInfo } /> }
 			</div>
 		);
 	}
